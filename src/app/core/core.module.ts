9933abafabd0340bba5ff936423cb725c09e8527
Michael Ang <angmerica@gmail.com>
2017-01-15 15:16:59 -0500

    add a not found page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,19 @@
 import { NgModule, Optional, SkipSelf }       from '@angular/core';
-
 import { CommonModule }      from '@angular/common';
 
 import { HeaderComponent }    from './header/header.component';
+import { NotFoundComponent }    from './not-found/not-found.component';
 
 @NgModule({
   imports:      [ CommonModule ],
-  declarations: [ HeaderComponent ],
-  exports:      [ HeaderComponent ]
+  declarations: [
+    HeaderComponent,
+    NotFoundComponent
+  ],
+  exports:      [
+    HeaderComponent,
+    NotFoundComponent
+  ]
 })
 export class CoreModule {
 
