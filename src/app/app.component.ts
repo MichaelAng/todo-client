76a5a32529144878b70d74e3739328e2104dab33
Michael Ang <angmerica@gmail.com>
2017-01-15 15:26:53 -0500

    rename css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ConfirmationService, Message } from 'primeng/primeng';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   providers: [ConfirmationService]
 })
 export class AppComponent {
