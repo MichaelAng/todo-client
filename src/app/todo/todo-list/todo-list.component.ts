17a70aef36e1a1e8e3b38f24c05ff0508286b793
Michael Ang <angmerica@gmail.com>
2017-01-21 01:06:39 -0500

    created a list of todos and iterated over it

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from '../models/todo.model';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  private todo: Todo[] = [
+    { id: 1, title: 'somename', complete: false },
+    { id: 1, title: 'somename', complete: false },
+    { id: 1, title: 'somename', complete: false },
+    { id: 1, title: 'somename', complete: false },
+    { id: 1, title: 'somename', complete: false }
+  ];
 
   constructor() { }
 
