091ff4e289a1807574eaaba1f07e5417c88718c5
Michael Ang <angmerica@gmail.com>
2017-01-30 13:45:52 -0500

    giving kubectl permission

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
+# sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment todo-client -p '{"spec":{"template":{"spec":{"containers":[{"name":"todo-client","image":"us.gcr.io/todo-client/hello:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"todo-client","image":"angmerica/todo-client:develop"}]}}}}'
