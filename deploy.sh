d8843ece86e7288fe2112862e781f084545ca527
Michael Ang <angmerica@gmail.com>
2017-02-01 11:58:26 -0500

    add image pull policy to always pull image

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 set -e
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$PROJECT_NAME'","image":"'$1'"}]}}}}'
+kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$PROJECT_NAME'","image":"'$1'", "imagePullPolicy":"Always"}]}}}}'
