c25858c362ff498f465ae4cefde041846bc9c912
Michael Ang <angmerica@gmail.com>
2017-01-31 20:15:50 -0500

    remove comment

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 # Exit on any error
 set -e
 
-# sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$PROJECT_NAME'","image":"'$1'"}]}}}}'
