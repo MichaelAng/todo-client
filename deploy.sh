d4ec0fafb05add49f3ad95d24aaf623f40596804
Michael Ang <angmerica@gmail.com>
2017-01-31 19:23:33 -0500

    turn the name and image into variable

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 
 # sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"todo-client","image":"angmerica/todo-client:k8s"}]}}}}'
+kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"$PROJECT_NAME","image":"$1"}]}}}}'
