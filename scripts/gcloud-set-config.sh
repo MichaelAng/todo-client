a885552c15403d9e63ecc739dc19da1f0dbe477c
Michael Ang <angmerica@gmail.com>
2017-01-31 20:15:13 -0500

    add google auth and set configurations

---
+++ scripts/gcloud-set-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Set Google configurations
+if [ $CIRCLE_BRANCH = 'develop' ]; then
+    CLUSTER_NAME_ENV = $CLUSTER_NAME
+elif [ $CIRCLE_BRANCH = 'master' ]; then
+    CLUSTER_NAME_ENV = $CLUSTER_NAME
+else
+    CLUSTER_NAME_ENV = $CLUSTER_NAME
+fi
+
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $PROJECT_NAME
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME_ENV
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME_ENV
