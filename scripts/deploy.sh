b88ddd1c294a5d4bedd11b046d07e69c132d5320
Michael Ang <angmerica@gmail.com>
2017-02-01 19:32:42 -0500

    add several comments for the shell scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# The deploy shell script updates the application's environment cluster google container engine.
+# The purpose is so CircleCI can automatically update a cluster container once the build process succeeds
+# This shell script is to be used in conjunction with circle.yml.
+#
+# Known Issues:
+# The pod will NOT fetch the latest docker image. This creates the preception that the pod did NOT get
+# refreshed, when in fact it did; it used a stale docker image. To correct the problem, you will need
+# to manually refresh the pod.
+#
+# To use this file:
+# ./deploy.sh [Pass in docker build image]
 
 # Exit on any error
 set -e
 
+# This command gives the ubuntu system's kube permissions.  This allows us to use the kubectl cli
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+# This tells the container cluster's to use the latest image.
 kubectl patch deployment $PROJECT_NAME -p '{"spec":{"template":{"spec":{"containers":[{"name":"'$PROJECT_NAME'","image":"'$1'", "imagePullPolicy":"Always"}]}}}}'
--- scripts/gcloud-set-config.sh
+++ scripts/gcloud-set-config.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# The gcloud-set-config shell script sets the gcloud default parameters.
+# This is necessary to point to the correct project, zone, and cluster
+# This shell script is to be used in conjunction with circle.yml.
+#
+# To use this file:
+# ./gcloud-set-config.sh
 
 # Exit on any error
 set -e
 
-# Set Google configurations
+# Detects the current branch to determine which cluster environment to point to.
 if [ $CIRCLE_BRANCH = 'develop' ]; then
     CLUSTER_NAME_ENV=$CLUSTER_NAME-develop
 elif [ $CIRCLE_BRANCH = 'master' ]; then
@@ -12,6 +18,7 @@ else
     CLUSTER_NAME_ENV=$CLUSTER_NAME
 fi
 
+# Sets the default gcloud configurations
 sudo /opt/google-cloud-sdk/bin/gcloud config set project $PROJECT_NAME
 sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME_ENV
 sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
