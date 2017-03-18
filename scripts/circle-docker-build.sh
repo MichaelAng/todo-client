d5d571e4bc76fbab49c7c81308a4e2bce393e38a
Michael Ang <angmerica@gmail.com>
2017-03-18 13:34:15 -0400

    remove special tag

--- scripts/circle-docker-build.sh
+++ scripts/circle-docker-build.sh
@@ -11,13 +11,13 @@ HOMEPATH=$HOME/todo-client
 if [ $CIRCLE_BRANCH = 'master' ]; then
     docker build -t angmerica/todo-client:api-production $HOMEPATH/api/
     docker build -t angmerica/todo-client:client-production $HOMEPATH/client/
-    docker build -f $HOMEPATH/webserver/Dockerfile.dist -t angmerica/todo-client:webserver-production $HOMEPATH/webserver/
+    docker build -t angmerica/todo-client:webserver-production $HOMEPATH/webserver/
 elif [ $CIRCLE_BRANCH = 'staging' ]; then
     docker build -t angmerica/todo-client:api-staging $HOMEPATH/api/
     docker build -t angmerica/todo-client:client-staging $HOMEPATH/client/
-    docker build -f $HOMEPATH/webserver/Dockerfile.dist -t angmerica/todo-client:webserver-staging $HOMEPATH/webserver/
+    docker build -t angmerica/todo-client:webserver-staging $HOMEPATH/webserver/
 else
     docker build -t angmerica/todo-client:api-develop $HOMEPATH/api/
     docker build -t angmerica/todo-client:client-develop $HOMEPATH/client/
-    docker build -f $HOMEPATH/webserver/Dockerfile.dist -t angmerica/todo-client:webserver-develop $HOMEPATH/webserver/
+    docker build -t angmerica/todo-client:webserver-develop $HOMEPATH/webserver/
 fi
