9fee97e2c6cf07a1e78b463783e02013bb4f2758
Michael Ang <angmerica@gmail.com>
2017-03-18 13:12:55 -0400

    made webserver work just like ddws in terms of build process

---
+++ webserver/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# NODE_ENV is the environment. It can accept integration or staging
+# KEY is the key you want to unlock the file with
+# To use
+# NODE_ENV=integration KEY=**** ./startup.sh
+
+# Exit on any error
+set -e
+echo THE ENVIRONMENT is $NODE_ENV
+[ "${NODE_ENV:=NODE_ENV=development}" ]
+if [ $NODE_ENV = "integration" ] || [ $NODE_ENV = "staging" ] || [ $NODE_ENV = "production" ]; then
+    nginx -c /etc/nginx/nginx.remote.conf
+else
+    nginx -c /etc/nginx/nginx.development.conf
+fi
