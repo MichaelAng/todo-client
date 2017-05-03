e1786a651b6987a18e28591fe112f94657869a7c
Michael Ang <angmerica@gmail.com>
2017-05-03 19:28:06 -0400

    correct syntax in the api and change service name from mongodb to mongo

--- api/startup.sh
+++ api/startup.sh
@@ -3,17 +3,17 @@
 
 [ "${NODE_ENV:=NODE_ENV=development}" ]
 
-# Wait for mongodb to be ready before starting the api
+# Wait for mongo to be ready before starting the api
 if [ $NODE_ENV = "integration" ] || [ $NODE_ENV = "staging" ] || [ $NODE_ENV = "production" ]; then
     echo "Starting in $NODE_ENV"
     node main.js
 else
     echo "Starting in $NODE_ENV"
-    while ! nc -z mongodb 27017; do
-        echo "mongodb not available sleeping for 3 seconds."
+    while ! nc -z mongo 27017; do
+        echo "mongo not available sleeping for 3 seconds."
         sleep 3;
     done;
-    echo 'mongodb available'
+    echo "mongo available"
     # Start the api
     pm2-docker --watch start main.js
 fi
